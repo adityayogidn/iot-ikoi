a9abc8826cfae98f6ce90aea2a58ac3a9047d6c3
adityayogidn <aditya.yogi1987@gmail.com>
2020-02-19 23:24:42 +0700

    update

---
+++ app/src/main/java/com/example/ikoi/WelcomeActivity.java
@@ -0,0 +1,30 @@
+package com.example.ikoi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        //menghilangkan Action Bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_welcome);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 3000L);
+    }
+}
