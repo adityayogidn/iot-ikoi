195a96acbd723e1db137acba5781c63a2ed0b42e
adityayogidn <aditya.yogi1987@gmail.com>
2020-03-10 20:30:23 +0700

    add project

--- app/src/main/java/com/example/ikoi/ControlingActivity.java
+++ app/src/main/java/com/example/ikoi/ControlingActivity.java
@@ -4,11 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.Objects;
+
 public class ControlingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_controling);
+
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(getString(R.string.app_name));
+    }
+
+    @Override
+    public boolean onSupportNavigateUp(){
+        onBackPressed();
+        return true;
+    }
+
+    @Override
+    public void onBackPressed(){
+        super.onBackPressed();
     }
 }
