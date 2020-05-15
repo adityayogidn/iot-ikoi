3eb2622176d46ff27faa00d93e48ca52ed62005f
adityayogidn <aditya.yogi1987@gmail.com>
2020-05-15 18:29:56 +0700

    update notification

--- app/src/main/java/com/example/ikoi/MonitoringActivity.java
+++ app/src/main/java/com/example/ikoi/MonitoringActivity.java
@@ -9,6 +9,7 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
@@ -123,10 +124,16 @@ public class MonitoringActivity extends AppCompatActivity {
                 int value = dataSnapshot.getValue(Integer.class);
                 if (value == 1){
                     nilaiKualitasAir.setText("Normal");
+                    nilaiKualitasAir.setTextColor(Color.parseColor("#03A9F4"));
+                    notification_normal();
                 } else if (value == 2) {
                     nilaiKualitasAir.setText("Warning");
+                    nilaiKualitasAir.setTextColor(Color.parseColor("#FFC107"));
+                    notification_warning();
                 } else if(value == 3) {
                     nilaiKualitasAir.setText("Danger");
+                    nilaiKualitasAir.setTextColor(Color.parseColor("#FF1807"));
+                    notification_danger();
                 } else {
                     nilaiKualitasAir.setText("Error");
                 }
@@ -140,31 +147,88 @@ public class MonitoringActivity extends AppCompatActivity {
 
     }
 
-    private void notification(){
+    private void notification_normal(){
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
             NotificationChannel channel =
                     new NotificationChannel("n", "n", NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setShowBadge(true);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+        Intent intent = new Intent(this, MonitoringActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "NotifApps")
+                .setContentTitle("Kualitas Air")
+                .setContentText("Status Kolam NORMAL")
+                .setAutoCancel(true)
+                .setSmallIcon(R.drawable.ic_fish)
+                .setAutoCancel(true) // menghapus notif ketika user melakukan tap pada notif
+                .setLights(200,200,200)
+                .setSound(Settings.System.DEFAULT_NOTIFICATION_URI)
+                .setOnlyAlertOnce(true)
+                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(pendingIntent); // action notif ketika di tap
+        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(this);
+        managerCompat.notify(1,builder.build());
+    }
+
+    private void notification_warning(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel =
+                    new NotificationChannel("n", "n", NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setShowBadge(true);
             NotificationManager manager = getSystemService(NotificationManager.class);
             manager.createNotificationChannel(channel);
         }
 
         Intent intent = new Intent(this, MonitoringActivity.class);
-        //intent.putExtras(bundle);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
                 PendingIntent.FLAG_ONE_SHOT);
 
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "n")
                 .setContentTitle("Kualitas Air")
-                .setContentText("Kualitas Air Kolam NORMAL")
+                .setContentText("Status Kolam WARNING")
                 .setAutoCancel(true)
-                .setSmallIcon(R.drawable.ic_fish) // icon
+                .setSmallIcon(R.drawable.ic_fish)
                 .setAutoCancel(true) // menghapus notif ketika user melakukan tap pada notif
-                .setLights(200,200,200) // light button
-                .setSound(Settings.System.DEFAULT_NOTIFICATION_URI) // set sound
-                .setOnlyAlertOnce(true) // set alert sound notif
+                .setLights(200,200,200)
+                .setSound(Settings.System.DEFAULT_NOTIFICATION_URI)
+                .setOnlyAlertOnce(true)
                 .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
-                //.setStyle(new NotificationCompat.BigTextStyle().bigText(message))
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(pendingIntent); // action notif ketika di tap
+        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(this);
+        managerCompat.notify(1,builder.build());
+    }
+
+    private void notification_danger(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel =
+                    new NotificationChannel("n", "n", NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setShowBadge(true);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+
+        Intent intent = new Intent(this, MonitoringActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "n")
+                .setContentTitle("Kualitas Air")
+                .setContentText("Status Kolam DANGER")
+                .setAutoCancel(true)
+                .setSmallIcon(R.drawable.ic_fish)
+                .setAutoCancel(true) // menghapus notif ketika user melakukan tap pada notif
+                .setLights(200,200,200)
+                .setSound(Settings.System.DEFAULT_NOTIFICATION_URI)
+                .setOnlyAlertOnce(true)
+                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setContentIntent(pendingIntent); // action notif ketika di tap
         NotificationManagerCompat managerCompat = NotificationManagerCompat.from(this);
